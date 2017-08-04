7b7e6e94ff563b33105ef38fb147607f4a2b90aa
Sergey <21102027+Dessummiis@users.noreply.github.com>
2017-08-05 00:12:53 +0300

    1st commit

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+i = 0
+
+while True:
+    i += 1
+    if i%3 == 0 and i%5 ==0:
+        print("FizzBuzz")
+    if i%3 == 0:
+        print("Fizz")
+    elif i%5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+    
+    if i == 100:
+        print("stop")
+        break
+    
