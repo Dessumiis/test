0370655e7587ebf9f98ead45340f3ef3ad9bed43
Sergey <dessummiis@users.noreply.github.com>
2017-08-05 00:33:31 +0300

    update

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,11 +2,11 @@ i = 0
 
 while True:
     i += 1
-    if i%3 == 0 and i%5 ==0:
+    if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
-    if i%3 == 0:
+    if i % 3 == 0:
         print("Fizz")
-    elif i%5 == 0:
+    elif i % 5 == 0:
         print("Buzz")
     else:
         print(i)
